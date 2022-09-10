072072b95da6ed328e0373881553aeb87f79dfe9
mukiliderrick <derrickmukili64@gmail.com>
2022-09-10 03:34:56 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 
 /*
- *main - prints Programming is like building a multilingualppuzzle
+ *main - prints the phrase Programming is like building a multilingual puzzle
  *return: prints 0 always
  *
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
