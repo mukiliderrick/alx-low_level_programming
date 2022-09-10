96c87f6cccd146de37634eb49d8b6c7d8381653b
mukiliderrick <derrickmukili64@gmail.com>
2022-09-10 05:17:28 -0700

    yes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
- *main- prints Your program should compile without warning when using the -Wall gcc option
- *return: a non zero
+ *  * main - prints the phrase with proper grammar,
  *
+ *   * but the outcome is a piece of art,
  *
- */
+ *    * Return: 0 if exited properly, non-zero otherwise
+ *
+ *     */
+
 int main(void)
+
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,29 @@
-#include<stdio.h>
+#include <stdio.h>
+
+
+
 /**
- *main - prints size of various data types
- *return: zero if executed properly
  *
+ *  * main - prints the size of various types
  *
- */
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
 
 int main(void)
+
 {
-	printf("Size of char: %d byte(s)\n",sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of an long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of an long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of an float: %d byte(s)\n", sizeof(float));
-	return 0;
+
+		printf("Size of a char: %d byte(s)\n", sizeof(char));
+
+			printf("Size of an int: %d byte(s)\n", sizeof(int));
+
+				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+
+					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+
+						printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+							return (0);
+
 }
