55c5c5bfaeea92e81dcd8e58452e503e600d3ac8
mukiliderrick <derrickmukili64@gmail.com>
2022-09-15 21:35:35 +0300

    nested loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,10 @@
  * _isupper checks for upper case
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c)
+int main(void)
 {
-    return (c >= 'A' && c<= 'Z');
+    int _isupper(int c)
+    {
+        return (c >= 'A' && c<= 'Z');
+    }
 }
