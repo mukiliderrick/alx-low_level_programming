5e196c1e333718e280f725a84683ce0589a895fa
mukiliderrick <derrickmukili64@gmail.com>
2022-09-15 21:25:07 +0300

    nested loop

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ * _isupper checks for upper case
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+    return (c >= 'A' && c<= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef main
+#define main
+
+int _isupper(int c);
+
+#endif
