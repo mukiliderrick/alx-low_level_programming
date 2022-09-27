cc4e9dfcd8d011c14c2890536e069622d14a0f44
mukiliderrick <derrickmukili64@gmail.com>
2022-09-27 15:29:37 +0300

    pointer and arrays

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
@@ -11,3 +13,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 #endif
+
+//gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-memset.c -o 0-memset
