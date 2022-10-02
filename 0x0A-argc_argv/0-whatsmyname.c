01ec28136a675ca65d22adc38777973d23e6416d
mukiliderrick <derrickmukili64@gmail.com>
2022-10-02 10:55:50 +0300

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Prints name of the program
+ * 
+ * @argc: size of the argv array and number of command line arguments
+ * 
+ * @argv: An array of size argc
+ * 
+ * Return: 0
+ */
+
+int main(int argc __attribute((unused)), char *argv[])
+{
+    printf("%s\n", argv[0]);
+    return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc __attribute((unused)), char *argv[]);
+
+#endif 
