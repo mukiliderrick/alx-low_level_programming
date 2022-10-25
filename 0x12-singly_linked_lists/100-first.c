de9007f1af6a09a7e67dbc45392353510fe8e9ed
mukiliderrick <derrickmukili64@gmail.com>
2022-10-25 09:53:29 -0700

    fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@
 void __attribute__((constructor)) calledFirst();
 void calledFirst()
 {
-	printf("You're beat! and yet, you must allow,\n")
+	printf("You're beat! and yet, you must allow,\n");
 	printf("bore my house upon my back!\n");
 }
