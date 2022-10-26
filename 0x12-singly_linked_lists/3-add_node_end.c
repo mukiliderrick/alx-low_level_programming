681abdfa5e338bd73e5646a0e04cf27504f875d9
mukiliderrick <derrickmukili64@gmail.com>
2022-10-25 23:14:04 -0700

    add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -8,41 +8,38 @@
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  list_t *last_node;
-  list_t *last = *head;
+	list_t *last_node;
+	list_t *last = *head;
 
-  unsigned int len =0;
+	unsigned int len = 0;
 
-  while(str[len])
-  {
-    len++;
-  }
+	while(str[len])
+	{
+		len++;
+	}
 
-    last_node = malloc(sizeof(list_t));
+	last_node = malloc(sizeof(list_t));
 
-    if (last_node == NULL)
-        return NULL;
+	if (last_node == NULL)
+		return NULL;
 
-    last_node->str = strdup(str);
-    last_node->len = len;
-    last_node->next = NULL;
+	last_node->str = strdup(str);
+	last_node->len = len;
+	last_node->next = NULL;
 
-    if(*head == NULL)
-    {
-        *head = last_node;
-        return(last_node);
-    }
-    else
-    {
-        while (last->next != NULL)
-        {
-            last = last->next;
-            last->next = last_node;
-            (*head)=last_node;
-        }
-        
-    }
-    return (last_node);
-
-
+	if(*head == NULL)
+	{
+		*head = last_node;
+		return(last_node);
+	}
+	else
+	{
+		while (last->next != NULL)
+		{
+			last = last->next;
+			last->next = last_node;
+			(*head)=last_node;
+		}
+	}
+	return (last_node);
 }
