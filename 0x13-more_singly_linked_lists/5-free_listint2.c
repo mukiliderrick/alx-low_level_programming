49242ad1cae9d4a13850e683303eb1bd92f6a240
mukiliderrick <derrickmukili64@gmail.com>
2022-10-28 01:32:17 -0700

    commited

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,19 @@
 #include "lists.h"
+
 /**
- * 
+ * free_listint2 - function that free a listint_t in list
+ * @hea: pointer to the head.
  */
 void free_listint2(listint_t **head)
 {
-    listint_t *new, *n;
-    new = *head;
-    
-    while (new)
-    {
-        n = new->next;
-        free(new);
-        new = n;
-    }
-    *head = NULL;
+	listint_t *new, *n;
+	new = *head;
+
+	while (new)
+	{
+		n = new->next;
+		free(new);
+		new = n;
+	}
+	*head = NULL;
 }
