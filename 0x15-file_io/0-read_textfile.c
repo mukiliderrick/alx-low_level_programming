79ac148f37ca4334fa06fc134d8abb8126092061
mukiliderrick <derrickmukili64@gmail.com>
2022-11-03 02:50:50 -0700

    fixed issues

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h> 
+#include <fcntl.h>
 #include <stdlib.h>
 
 /**
@@ -18,22 +18,29 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buff;
-	ssize_t reading;
+	ssize_t read_only;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open("filename", O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		printf("Failed to read only\n");
 		return (0);
 	}
-	buff = malloc(sizeof(buff));
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
 
-	reading = read (fd, buff, letters);
+	read_only = read(fd, buff, letters);
+	if (read_only == -1)
+	{
+		free(buff);
+		close(fd);
+		return (0);
+	}
+
 	close(fd);
-	return (reading);
+	return (read_only);
 }
