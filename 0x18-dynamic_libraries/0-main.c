4314b90e149dcef62fce571b48dc27c073d999c9
mukiliderrick <derrickmukili64@gmail.com>
2022-12-13 09:52:47 -0800

    complete

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *
+ *	main - check the code
+ *
+ *	Return: Always EXIT_SUCCESS.
+ *
+ */
+
+int main(void)
+{
+	    printf("%d\n", strlen("My Dyn Lib"));
+	        return (EXIT_SUCCESS);
+}
