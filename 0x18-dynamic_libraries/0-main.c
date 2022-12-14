bf766e2d8a292f17d09e0ef91fcd30357e910e27
mukiliderrick <derrickmukili64@gmail.com>
2022-12-13 22:57:54 -0800

    task 1

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,12 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+
 /**
+ * main - check the code
  *
- *	main - check the code
- *
- *	Return: Always EXIT_SUCCESS.
- *
+ * Return: Always EXIT_SUCCESS.
  */
 
 int main(void)
